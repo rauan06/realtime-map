89d80c6f1525c7a9ab665bd02b6462009987c96c
Rauan <ommirkulr@gmail.com>
2025-09-03 16:39:43 +0500

    small changes

--- cord-receiver/controller/grpc.go
+++ cord-receiver/controller/grpc.go
@@ -7,27 +7,27 @@ import (
 	"net"
 	"net/http"
 
-	"github.com/rauan06/realtime-map/go-commons/gen/proto/cord-receiver"
+	receiverpb "github.com/rauan06/realtime-map/go-commons/gen/proto/cord-receiver"
 
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
-	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 )
 
 // Implement the service
 type locationServer struct {
-	locationpb.UnimplementedLocationServiceServer
+	receiverpb.UnimplementedLocationServiceServer
 }
 
-func (s *locationServer) SendLocation(ctx context.Context, req *locationpb.GpsData) (*locationpb.LocationResponse, error) {
+func (s *locationServer) SendLocation(ctx context.Context, req *receiverpb.GpsData) (*receiverpb.LocationResponse, error) {
 	fmt.Printf("Received location: lat=%.6f, lon=%.6f, ts=%d\n", req.Latitude, req.Longitude, req.Timestamp)
-	return &locationpb.LocationResponse{Status: "OK"}, nil
+	return &receiverpb.LocationResponse{Status: "OK"}, nil
 }
 
 func main() {
 	// Start gRPC server
 	grpcServer := grpc.NewServer()
-	locationpb.RegisterLocationServiceServer(grpcServer, &locationServer{})
+	receiverpb.RegisterLocationServiceServer(grpcServer, &locationServer{})
 	reflection.Register(grpcServer)
 
 	lis, err := net.Listen("tcp", ":50051")
@@ -47,7 +47,7 @@ func main() {
 	ctx := context.Background()
 	opts := []grpc.DialOption{grpc.WithInsecure()}
 
-	err = locationpb.RegisterLocationServiceHandlerFromEndpoint(ctx, mux, "localhost:50051", opts)
+	err = receiverpb.RegisterLocationServiceHandlerFromEndpoint(ctx, mux, "localhost:50051", opts)
 	if err != nil {
 		log.Fatalf("failed to start HTTP gateway: %v", err)
 	}
