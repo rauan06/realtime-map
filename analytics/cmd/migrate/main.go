e152fd7a3014d7d59af4b71cfbaa956ad89b270e
Rauan <ommirkulr@gmail.com>
2025-09-15 16:42:54 +0500

    add migrations for analytics

--- analytics/cmd/migrate/main.go
+++ analytics/cmd/migrate/main.go
@@ -1,1 +1,74 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: migrator [up|down|drop|force <version>|version]")
+		os.Exit(1)
+	}
+
+	m, err := migrate.New(
+		"file://migrations",
+		"postgres://postgres:example@localhost:5431/realtimedb?sslmode=disable",
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cmd := os.Args[1]
+
+	switch cmd {
+	case "up":
+		if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+			log.Fatal(err)
+		}
+		fmt.Println("Migrations applied successfully")
+
+	case "down":
+		if err := m.Steps(-1); err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("Rolled back one migration")
+
+	case "drop":
+		if err := m.Drop(); err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("Database dropped")
+
+	case "force":
+		if len(os.Args) < 3 {
+			log.Fatal("force requires version number")
+		}
+		version, err := strconv.Atoi(os.Args[2])
+		if err != nil {
+			log.Fatal("invalid version number")
+		}
+		if err := m.Force(version); err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("Forced to version %d\n", version)
+
+	case "version":
+		v, dirty, err := m.Version()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("Current version: %d (dirty: %t)\n", v, dirty)
+
+	default:
+		fmt.Println("Unknown command:", cmd)
+		fmt.Println("Usage: migrator [up|down|drop|force <version>|version]")
+		os.Exit(1)
+	}
+}
--- analytics/go.mod
+++ analytics/go.mod
@@ -5,6 +5,7 @@ go 1.24.6
 require (
 	github.com/caarlos0/env/v11 v11.3.1
 	github.com/confluentinc/confluent-kafka-go/v2 v2.11.1
+	github.com/golang-migrate/migrate/v4 v4.19.0
 	github.com/joho/godotenv v1.5.1
 	github.com/rauan06/realtime-map/go-commons v0.0.0-20250914130302-25db61708726
 	github.com/redis/go-redis/v9 v9.13.0
@@ -13,13 +14,14 @@ require (
 require (
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
-	github.com/docker/docker v27.3.0+incompatible // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/lib/pq v1.10.9 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
-	github.com/moby/sys/userns v0.1.0 // indirect
+	github.com/moby/go-archive v0.1.0 // indirect
+	github.com/moby/sys/atomicwriter v0.1.0 // indirect
+	github.com/moby/sys/user v0.4.0 // indirect
 	github.com/rs/zerolog v1.34.0 // indirect
-	go.opentelemetry.io/otel v1.37.0 // indirect
-	go.opentelemetry.io/otel/metric v1.37.0 // indirect
-	go.opentelemetry.io/otel/trace v1.37.0 // indirect
 	golang.org/x/sys v0.35.0 // indirect
 )
